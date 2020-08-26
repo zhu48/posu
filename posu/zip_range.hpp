6fc6c8be19b0a8a48eab471154b1f6caeb8fb2f3
Zuodian Hu <huzuodian@gmail.com>
2020-08-26 16:39:29 -0500

    feat(ranges): zip_range header intial commit

---
+++ posu/zip_range.hpp
@@ -0,0 +1,12 @@
+#ifndef POSU_ZIP_RANGE_HPP
+#define POSU_ZIP_RANGE_HPP
+
+#include <ranges>
+
+namespace posu {
+
+    class zip_range {};
+
+} // namespace posu
+
+#endif // #ifndef POSU_ZIP_RANGE_HPP
