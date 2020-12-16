735564a5faa686587e7018e2a10b5f7c5eafb52a
Zuodian Hu <huzuodian@gmail.com>
2020-12-15 22:01:50 -0600

    feat(types): `type_list::at`

--- posu/type_list.hpp
+++ posu/type_list.hpp
@@ -1,10 +1,15 @@
 #ifndef POSU_TYPE_LIST_HPP
 #define POSU_TYPE_LIST_HPP
 
+#include <tuple>
+
 namespace posu {
 
     template<typename... Types>
     struct type_list {
+    public:
+        template<std::size_t I>
+        using at = std::tuple_element_t<I, std::tuple<Types...>>;
     };
 
 } // namespace posu
--- test/test_type_list.cpp
+++ test/test_type_list.cpp
@@ -7,9 +7,13 @@
 
 TEST_CASE("initialization", "[construct]")
 {
-    using list = posu::type_list<int, float, double>;
+    using list = posu::type_list<int, float, double, int&, float&&, const double&>;
 
     static_assert(std::same_as<list::at<0>, int>, "the first element must be int");
     static_assert(std::same_as<list::at<1>, float>, "the second element must be float");
     static_assert(std::same_as<list::at<2>, double>, "the third element must be double");
+    static_assert(std::same_as<list::at<3>, int&>, "the third element must be int&");
+    static_assert(std::same_as<list::at<4>, float&&>, "the third element must be float&&");
+    static_assert(
+        std::same_as<list::at<5>, const double&>, "the third element must be const double&");
 }
