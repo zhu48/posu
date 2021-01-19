4ad284f60cae6d80c9ad3ed027f2870c996e145e
Zuodian Hu <huzuodian@gmail.com>
2021-01-18 21:09:07 -0600

    fix(test): test title and tags

--- test/test_type_ratio.cpp
+++ test/test_type_ratio.cpp
@@ -3,7 +3,10 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEMPLATE_TEST_CASE("", "", (std::tuple<posu::type_ratio<>, posu::type_ratio<>, posu::type_ratio<>>))
+TEMPLATE_TEST_CASE(
+    "multiplication",
+    "[ratio][multiply]",
+    (std::tuple<posu::type_ratio<>, posu::type_ratio<>, posu::type_ratio<>>))
 {
     using lhs     = std::tuple_element_t<0, TestType>;
     using rhs     = std::tuple_element_t<1, TestType>;
