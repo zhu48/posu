0832f5f8cf8ac88dbb08f44d8ecbec2ac401a0a2
Zuodian Hu <huzuodian@gmail.com>
2021-01-19 22:24:17 -0600

    docs(types): `type_ratio` header documentation

--- posu/type_ratio.hpp
+++ posu/type_ratio.hpp
@@ -7,12 +7,18 @@
 
 namespace posu {
 
+    /**
+     * @brief A ratio between type products.
+     *
+     * @tparam Numerator   The list of types in the numerator.
+     * @tparam Denominator The list of types in the denominator.
+     */
     template<typename Numerator = type_list<>, typename Denominator = type_list<>>
     struct type_ratio {
-        using num = Numerator;
-        using den = Denominator;
+        using num = Numerator;   //!< The numerator type list.
+        using den = Denominator; //!< The denominator type list.
 
-        using type = type_ratio<num, den>;
+        using type = type_ratio<num, den>; //!< Self-alias.
     };
 
     namespace detail {
@@ -22,6 +28,12 @@ namespace posu {
 
     } // namespace detail
 
+    /**
+     * @brief Multiply two type ratios together.
+     *
+     * @tparam Lhs The left-hand-size operand.
+     * @tparam Rhs The right-hand-side operand.
+     */
     template<typename Lhs, typename Rhs>
     using ratio_multiply = typename detail::ratio_multiply_impl<Lhs, Rhs>::type;
 
