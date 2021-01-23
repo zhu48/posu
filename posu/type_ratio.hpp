eef67c267775c36217806f942d421147581f18ee
Zuodian Hu <zhu48@wisc.edu>
2021-01-23 17:07:29 -0600

    feat(types): `type_ratio` (#18)
    * feat(types): concatenate two lists
    
    * refactor(types): abbreviated `using`
    
    * feat(types): variadic concatenation
    
    * feat(types): add to front and back of lists
    
    * docs(types): `prepend` and `append`
    
    * test(types): library-decay `append` and `prepend`
    
    * refactor(types): use `push` terminology
    
    * fix(test): `type_list` `push` test
    
    * feat(types): `pop_front` and `pop_back`
    
    * feat(types): `type_list` `first` sub-list
    
    * feat(types): `type_list` `last` sub-list
    
    * feat(types): `type_list` `find` type index
    
    * feat(types): `type_list` `insert`
    
    * feat(types): `type_list` `remove` by index
    
    * feat(types): `type_ratio` initial commit
    
    * test(types): `ratio_multiply` base case
    
    * fix(test): test title and tags
    
    * feat(types): identity multiplication
    
    * format(types): don't disable `requires` formatting
    
    * feat(types): `type_list` `size` and `ssize`
    
    * feat(types): `at` free meta-function
    
    * feat(types): `find_v`
    
    * feat(types): `contains` free meta-function
    
    * fix(types): badly constrained reduction logic
    
    * test(types): reduce multiplication
    
    * docs(types): `type_ratio` header documentation
    
    * feat(types): `ratio_divide`

---
+++ posu/type_ratio.hpp
@@ -0,0 +1,96 @@
+#ifndef POSU_TYPE_RATIO_HPP
+#define POSU_TYPE_RATIO_HPP
+
+#include <type_traits>
+
+#include "posu/type_list.hpp"
+
+namespace posu {
+
+    /**
+     * @brief A ratio between type products.
+     *
+     * @tparam Numerator   The list of types in the numerator.
+     * @tparam Denominator The list of types in the denominator.
+     */
+    template<typename Numerator = type_list<>, typename Denominator = type_list<>>
+    struct type_ratio {
+        using num = Numerator;   //!< The numerator type list.
+        using den = Denominator; //!< The denominator type list.
+
+        using type = type_ratio<num, den>; //!< Self-alias.
+    };
+
+    /**
+     * @brief Traits template to detect whether a type specializes `type_list` or not.
+     * @{
+     */
+    //! @tparam T The type to check.
+    template<typename T>
+    struct is_type_ratio : std::false_type {
+    };
+    //! @tparam Types The types in the type list.
+    template<typename Numerator, typename Denominator>
+    struct is_type_ratio<type_ratio<Numerator, Denominator>> : std::true_type {
+    };
+    template<typename T>
+    inline constexpr bool is_type_ratio_v = is_type_ratio<T>::value;
+    //! @}
+
+    /**
+     * @brief Obtain the numerator of the given type ratio.
+     *
+     * @tparam T The type ratio to get the numerator of.
+     */
+    template<typename T>
+        requires(is_type_ratio_v<T>)
+    using numerator = typename T::num;
+
+    /**
+     * @brief Obtain the denominator of the given type ratio.
+     *
+     * @tparam T The type ratio to get the denominator of.
+     */
+    template<typename T>
+        requires(is_type_ratio_v<T>)
+    using denominator = typename T::den;
+
+    /**
+     * @brief Obtain the inverse of the given type ratio.
+     *
+     * @tparam T The type ratio to get the inverse of.
+     */
+    template<typename T>
+        requires(is_type_ratio_v<T>)
+    using inverse = type_ratio<denominator<T>, numerator<T>>;
+
+    namespace detail {
+
+        template<typename Lhs, typename Rhs>
+        struct ratio_multiply_impl;
+
+    } // namespace detail
+
+    /**
+     * @brief Multiply two type ratios together.
+     *
+     * @tparam Lhs The left-hand-size operand.
+     * @tparam Rhs The right-hand-side operand.
+     */
+    template<typename Lhs, typename Rhs>
+    using ratio_multiply = typename detail::ratio_multiply_impl<Lhs, Rhs>::type;
+
+    /**
+     * @brief Divide one type ratios by another.
+     *
+     * @tparam Dividend The type ratio to divide.
+     * @tparam Divisor  The type ratio to divide by.
+     */
+    template<typename Dividend, typename Divisor>
+    using ratio_divide = ratio_multiply<Dividend, inverse<Divisor>>;
+
+} // namespace posu
+
+#include "posu/ipp/type_ratio.ipp"
+
+#endif // #ifndef POSU_TYPE_RATIO_HPP
