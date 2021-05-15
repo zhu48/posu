c82fd9f53a5dbf7070400c17d8ee5965f4239b71
Zuodian Hu <huzuodian@gmail.com>
2021-05-15 10:04:17 -0500

    feat(units): `dimension` and `kind` concepts

---
+++ posu/units/dimension.hpp
@@ -0,0 +1,61 @@
+#ifndef POSU_UNITS_DIMENSION_HPP
+#define POSU_UNITS_DIMENSION_HPP
+
+#include <string_view>
+
+#include "posu/concepts.hpp"
+#include "posu/type_algorithm.hpp"
+#include "posu/type_ratio.hpp"
+
+namespace posu::units {
+
+    template<typename T>
+    inline constexpr bool enable_as_dimension = false;
+
+    template<typename T>
+    concept base_dimension = meta_constant<T, std::string_view> && enable_as_dimension<T>;
+
+    template<typename T>
+    struct is_base_dimension : public std::bool_constant<base_dimension<T>> {
+    };
+
+    template<typename T>
+    concept derived_dimension = is_type_ratio_v<T> && posu::all_of<is_base_dimension>(
+        typename T::num{}) && posu::all_of<is_base_dimension>(typename T::den{});
+
+    template<typename T>
+    concept dimension = base_dimension<T> || derived_dimension<T>;
+
+    struct dimensionless {
+        using type       = dimensionless;
+        using value_type = std::string_view;
+
+        static constexpr auto value = std::string_view{"dimensionless"};
+
+        [[nodiscard]] constexpr auto operator()() const noexcept { return value; }
+        [[nodiscard]] constexpr      operator value_type() const noexcept { return value; }
+    };
+
+    namespace detail {
+
+        template<typename Lhs, typename Rhs>
+        struct dimension_multiply_impl;
+
+        template<typename Lhs, typename Rhs>
+        struct dimension_divide_impl;
+
+    } // namespace detail
+
+    template<dimension Lhs, dimension Rhs>
+    using dimension_multiply = typename detail::dimension_multiply_impl<Lhs, Rhs>::type;
+    template<dimension Lhs, dimension Rhs>
+    using dimension_divide = typename detail::dimension_divide_impl<Lhs, Rhs>::type;
+
+} // namespace posu::units
+
+template<>
+inline constexpr bool posu::units::enable_as_dimension<posu::units::dimensionless> = true;
+
+#include "posu/units/ipp/dimension.ipp"
+
+#endif // #ifndef POSU_UNITS_DIMENSION_HPP
---
+++ posu/units/kind.hpp
@@ -0,0 +1,61 @@
+#ifndef POSU_UNITS_KIND_HPP
+#define POSU_UNITS_KIND_HPP
+
+#include "posu/units/dimension.hpp"
+
+namespace posu::units {
+
+    template<typename T>
+    inline constexpr bool enable_as_kind = false;
+
+    template<typename T>
+    using dimension_t = typename T::dimensions;
+
+    template<typename T>
+    concept kind =
+        meta_constant<T, std::string_view> && dimension<dimension_t<T>> && enable_as_kind<T>;
+
+    struct scaler {
+        using type       = scaler;
+        using value_type = std::string_view;
+        using dimensions = dimensionless;
+
+        static constexpr auto value = std::string_view{"scaler"};
+
+        [[nodiscard]] constexpr auto operator()() const noexcept { return value; }
+        [[nodiscard]] constexpr      operator value_type() const noexcept { return value; }
+    };
+
+    template<dimension Dimension>
+    struct unknown {
+        using type       = scaler;
+        using value_type = std::string_view;
+        using dimensions = Dimension;
+
+        static constexpr auto value = std::string_view{"unknown"};
+
+        [[nodiscard]] constexpr auto operator()() const noexcept { return value; }
+        [[nodiscard]] constexpr      operator value_type() const noexcept { return value; }
+    };
+
+    template<kind Lhs, kind Rhs>
+    struct kind_multiply_result {
+        using type = unknown<dimension_multiply<dimension_t<Lhs>, dimension_t<Rhs>>>;
+    };
+
+    template<kind Num, kind Den>
+    struct kind_divide_result {
+        using type = unknown<dimension_divide<dimension_t<Num>, dimension_t<Den>>>;
+    };
+
+    template<kind Lhs, kind Rhs>
+    using kind_multiply = typename kind_multiply_result<Lhs, Rhs>::type;
+    template<kind Lhs, kind Rhs>
+    using kind_divide = typename kind_multiply_result<Lhs, Rhs>::type;
+
+} // namespace posu::units
+
+template<>
+inline constexpr bool posu::units::enable_as_kind<posu::units::scaler> = true;
+
+#endif // #ifndef POSU_UNITS_KIND_HPP
