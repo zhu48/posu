e200512f317f7b54b716202eb75dd6d424eb9a5b
Zuodian Hu <huzuodian@gmail.com>
2021-05-15 13:03:35 -0500

    feat(units): SI speed of light

---
+++ posu/units/system/si/defining_constants.hpp
@@ -0,0 +1,12 @@
+#ifndef POSU_UNITS_SYSTEM_SI_DEFINING_CONSTANTS_HPP
+#define POSU_UNITS_SYSTEM_SI_DEFINING_CONSTANTS_HPP
+
+#include "posu/units/system/si/speed.hpp"
+
+namespace posu::units::si {
+
+    inline constexpr auto speed_of_light = 299'792'458_m_per_sec;
+
+}
+
+#endif // #ifndef POSU_UNITS_SYSTEM_SI_DEFINING_CONSTANTS_HPP
