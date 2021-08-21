988a6460d60c800811647c3dcd6aa65dfdbdc208
Zuodian Hu <huzuodian@gmail.com>
2021-08-21 13:58:28 -0500

    feat(units): elementary charge

--- posu/units/system/si/defining_constants.hpp
+++ posu/units/system/si/defining_constants.hpp
@@ -1,6 +1,7 @@
 #ifndef POSU_UNITS_SYSTEM_SI_DEFINING_CONSTANTS_HPP
 #define POSU_UNITS_SYSTEM_SI_DEFINING_CONSTANTS_HPP
 
+#include "posu/units/system/si/electric_charge.hpp"
 #include "posu/units/system/si/energy.hpp"
 #include "posu/units/system/si/frequency.hpp"
 #include "posu/units/system/si/length.hpp"
@@ -11,6 +12,7 @@ namespace posu::units::si {
     inline constexpr auto cs_133_hyperfine_freq = 9'192'631'770_Hz;
     inline constexpr auto speed_of_light        = 299'792'458_m / s;
     inline constexpr auto planck_constant       = 6.626'070'15e-34_J * s;
+    inline constexpr auto elementary_charge     = 1.602'176'634e-19_C;
 
 } // namespace posu::units::si
 
