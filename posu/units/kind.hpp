b760c80832c8eda0404eac6ab60b0b6924cd1433
Zuodian Hu <huzuodian@gmail.com>
2022-01-09 12:16:56 -0600

    docs(units): `kind` header docs

--- posu/units/kind.hpp
+++ posu/units/kind.hpp
@@ -1,3 +1,17 @@
+/**
+ * @file posu/units/kind.hpp
+ *
+ * @brief Header defining a measurement `kind` concept.
+ *
+ * The Units library uses `kind` types to represent semantically specific measurements of some
+ * dimension. This header defines the `kind` concept, some ancillary types, and basic `kind`
+ * dimensional analysis operations.
+ *
+ * Each `kind` represents
+ * measurements of a particular dimension, but arbitrary different measurement kinds may associate
+ * with the same dimension.
+ */
+
 #ifndef POSU_UNITS_KIND_HPP
 #define POSU_UNITS_KIND_HPP
 
@@ -5,61 +19,96 @@
 
 namespace posu::units {
 
+    /**
+     * @brief Customization point for designating custom types as `kind` types.
+     *
+     * @tparam T The type to designate as a `kind` type.
+     */
     template<typename T>
     inline constexpr bool enable_as_kind = false;
 
+    /**
+     * @brief Meta-function to extract a type's dimension.
+     *
+     * @tparam T The type to extract the dimension of.
+     *
+     * @return Returns the given type's measurement dimension.
+     */
     template<typename T>
     using dimension_t = typename T::dimensions;
 
+    /**
+     * @brief A measurement kind type.
+     *
+     * A `kind` type is a `std::string_view` meta-constant designated as a `kind` type via
+     * specializing the `enable_as_kind` template. The syntactic requirements give a kind a name
+     * string, a unique type in the type system, and an associated dimension. Semantically, each
+     * `kind` is a tag type for semantically-distinct measurements of a particular dimension.
+     * Arbitrary different measurement kinds may associate with the same dimension.
+     *
+     * @tparam T The type to check against this concept.
+     */
     template<typename T>
     concept kind =
         meta_constant<T, std::string_view> && dimension<dimension_t<T>> && enable_as_kind<T>;
 
     /**
-     * @brief A unit-of-measure derived from base units.
+     * @brief A measurement kind measuring a derived dimension.
      *
      * @tparam T The type to check against this concept.
-     *
-     * @{
      */
     template<typename T>
     concept derived_kind = kind<T> && derived_dimension<dimension_t<T>>;
-    template<typename T>
-    struct is_derived_kind : public std::bool_constant<derived_kind<T>> {
-    };
-    //! @}
 
     /**
-     * @brief A base unit-of-measure.
+     * @brief A measurement kind measuring a base dimension.
      *
      * @tparam T The type to check against this concept.
-     *
-     * @{
      */
     template<typename T>
     concept base_kind = kind<T> && base_dimension<dimension_t<T>>;
-    template<typename T>
-    struct is_base_kind : public std::bool_constant<base_kind<T>> {
-    };
-    //! @}
 
+    /**
+     * @brief A scaler measurement kind, or dimensionless unit of measurement.
+     *
+     * @tparam Period A multiplier to apply to the scaler when used as a unit of measurement.
+     */
     template<detail::std_ratio Period = std::ratio<1>>
     struct scaler {
-        using type       = scaler;
-        using value_type = std::string_view;
-        using dimensions = dimensionless;
-        using kind_type  = scaler;
-        using period     = Period;
+        using type       = scaler;           //!< Self type.
+        using value_type = std::string_view; //!< Meta-constant value type.
+        using dimensions = dimensionless;    //!< Mark this type as dimensionless.
+        using kind_type  = scaler; //!< Mark this type as its own `kind` when used as a `unit`.
+        using period     = Period; //!< The dimensional multiplier for a dimensionless unit.
 
-        static constexpr auto value = std::string_view{"scaler"};
+        static constexpr auto value = std::string_view{"scaler"}; //!< The kind or unit name.
 
+        /**
+         * @name Value Access
+         *
+         * @brief Obtain this kind or unit's name.
+         *
+         * @return Returns this kind or unit's name.
+         *
+         * @{
+         */
         [[nodiscard]] constexpr auto operator()() const noexcept { return value; }
         [[nodiscard]] constexpr      operator value_type() const noexcept { return value; }
+        //! @}
     };
 
+    /**
+     * @brief Tag specialization designating `scaler` as a kind of measurement.
+     */
     template<detail::std_ratio Period>
     inline constexpr bool enable_as_kind<scaler<Period>> = true;
 
+    /**
+     * @brief An unknown measurement kind or unit of unknown kind.
+     *
+     * @tparam T      The known unit kind, or kind dimension.
+     * @tparam Period A multiplier to apply when used as a unit of measurement.
+     */
     template<typename T, detail::std_ratio Period = std::ratio<1>>
         requires(dimension<T> || kind<T>)
     struct unknown;
