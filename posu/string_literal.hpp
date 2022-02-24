39d668bd3d870c4b9661dba0eccad22b6c245288
Zuodian Hu <huzuodian@gmail.com>
2022-02-23 20:30:43 -0600

    docs(posu): `basic_string_literal` header docs

--- posu/string_literal.hpp
+++ posu/string_literal.hpp
@@ -5,35 +5,75 @@
 #include <utility>
 
 namespace posu {
+
+    /**
+     * @brief String literal type.
+     *
+     * @tparam CharT The string character type.
+     * @tparam N     The number of characters in the string.
+     */
     template<typename CharT, std::size_t N>
     struct basic_string_literal {
     private:
         using storage_type = CharT[N];
 
     public:
-        using value_type      = CharT;
-        using size_type       = std::size_t;
-        using const_reference = const value_type&;
-        using reference       = const_reference;
-        using const_pointer   = const value_type*;
-        using pointer         = const_pointer;
-        using iterator        = pointer;
-        using const_iterator  = const_pointer;
+        using value_type      = CharT;             //!< Character type.
+        using size_type       = std::size_t;       //!< String length type.
+        using const_reference = const value_type&; //!< Immutable character reference.
+        using reference       = const_reference;   //!< Mutable character reference.
+        using const_pointer   = const value_type*; //!< Immutable character pointer.
+        using pointer         = const_pointer;     //!< Mutable character pointer.
+        using iterator        = pointer;           //!< Mutable character iterator.
+        using const_iterator  = const_pointer;     //!< Immutable character iterator.
 
+        /**
+         * @brief Default-construct zero-length string literal.
+         */
         constexpr basic_string_literal() noexcept requires(N == 0) = default;
+
+        /**
+         * @brief Construct string literal from constant character array.
+         *
+         * @param arr A constant character array to initialize from.
+         */
         constexpr basic_string_literal(const CharT (&arr)[N]) noexcept;
 
-        [[nodiscard]] constexpr operator const storage_type&() const noexcept { return value; }
-
+        /**
+         * @name Literal Access
+         *
+         * @brief Access as a string literal.
+         *
+         * @return Returns the represented string literal.
+         *
+         * @{
+         */
+        [[nodiscard]] constexpr      operator const storage_type&() const noexcept { return value; }
         [[nodiscard]] constexpr auto data() const noexcept -> const_pointer { return value; }
         [[nodiscard]] constexpr auto c_str() const noexcept -> const_pointer { return value; }
+        //! @}
 
+        /**
+         * @brief Obtain the string literal length, including the null terminator.
+         *
+         * @return Returns the string literal length.
+         */
         [[nodiscard]] constexpr auto size() const noexcept -> size_type { return N; }
 
+        /**
+         * @name Iterators
+         *
+         * @brief Obtain iterators into the string literal.
+         *
+         * @return Returns the requested iterator.
+         *
+         * @{
+         */
         [[nodiscard]] constexpr auto begin() const noexcept -> const_iterator { return data(); }
         [[nodiscard]] constexpr auto cbegin() const noexcept -> const_iterator { return data(); }
         [[nodiscard]] constexpr auto end() const noexcept -> const_iterator { return data() + N; }
         [[nodiscard]] constexpr auto cend() const noexcept -> const_iterator { return data() + N; }
+        //! @}
 
         const storage_type value;
 
@@ -46,9 +86,22 @@ namespace posu {
         constexpr basic_string_literal(C... c) noexcept;
     };
 
+    /**
+     * @brief Deduction from string literal.
+     *
+     * @tparam CharT The string character type.
+     * @tparam N     The string literal length.
+     */
     template<typename CharT, std::size_t N>
     basic_string_literal(const CharT (&)[N]) -> basic_string_literal<std::remove_cv_t<CharT>, N>;
 
+    /**
+     * @brief Convenience aliases for common character types.
+     *
+     * @tparam N The number of characters in the literal.
+     *
+     * @{
+     */
     template<std::size_t N>
     using string_literal = basic_string_literal<char, N>;
     template<std::size_t N>
@@ -59,6 +112,7 @@ namespace posu {
     using u16string_literal = basic_string_literal<char16_t, N>;
     template<std::size_t N>
     using u32string_literal = basic_string_literal<char32_t, N>;
+    //! @}
 
 } // namespace posu
 
