9da485c7b74dd14ce81445495c53813b65d75c87
Zuodian Hu <huzuodian@gmail.com>
2022-02-27 22:21:43 -0600

    test(units): dimensional analysis outline

--- test/units/test_dimension.cpp
+++ test/units/test_dimension.cpp
@@ -19,4 +19,25 @@ CATCH_TEST_CASE("dimension definition results in operable tag types", "[units][t
 
         CATCH_STATIC_REQUIRE(units::dimension<test_dim>);
     }
+
+    CATCH_SECTION("dimensional analysis")
+    {
+        CATCH_SECTION("multiplication")
+        {
+            CATCH_SECTION("multiplication between base dimensions") {}
+
+            CATCH_SECTION("multiplication between derived dimensions") {}
+
+            CATCH_SECTION("multiplication between base and derived dimensions") {}
+        }
+
+        CATCH_SECTION("division")
+        {
+            CATCH_SECTION("division between base dimensions") {}
+
+            CATCH_SECTION("division between derived dimensions") {}
+
+            CATCH_SECTION("division between base and derived dimensions") {}
+        }
+    }
 }
